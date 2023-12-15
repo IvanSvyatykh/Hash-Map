942157f861869ed4984560ed3dd2500670c48141
informant <artemiy.solodkin@mail.ru>
2023-12-15 21:37:53 +0500

    1 done, 1 left

--- Hash-Map/HashFunctions/HashFucntionsForOpenAdress/HashFunc/CircularShiftHashForInt.cs
+++ Hash-Map/HashFunctions/HashFucntionsForOpenAdress/HashFunc/CircularShiftHashForInt.cs
@@ -30,22 +30,20 @@ namespace Hash_Map.HashFunctions.HashFucntionsForOpenAdress.HashFunc
 
         public string GetName()
         {
-            return "Хэш функции с seed-ом";
+            return "Хэш функции с побитовыми сдвигами и XOR";
         }
         private int HashFunc(object data, int size, int attemptNumber)
         {
-
-
-           int key = ((int)data);
+            int key = (int)data;
 
             key += ~(key << 16);
-            key ^= (key >> 5+attemptNumber);
+            key ^= (key >> 5);
             key += (key << 3);
-            key ^= (key >> 13 + attemptNumber);
+            key ^= (key >> 13);
             key += ~(key << 9);
-            key ^= (key >> 17 + attemptNumber);
+            key ^= (key >> 17);
 
-            return key%size;
+            return (key + attemptNumber) %size;
 
             //string key = ((int)data).ToString();
 
