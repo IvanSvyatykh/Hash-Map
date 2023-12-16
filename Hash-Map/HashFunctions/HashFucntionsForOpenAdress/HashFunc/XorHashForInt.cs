c2add53c5c043f60cd29bf63a5e1fb6a99d2b1be
informant <artemiy.solodkin@mail.ru>
2023-12-16 21:10:08 +0500

    Gost second step

--- Hash-Map/HashFunctions/HashFucntionsForOpenAdress/HashFunc/XorHashForInt.cs
+++ Hash-Map/HashFunctions/HashFucntionsForOpenAdress/HashFunc/XorHashForInt.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,15 @@ namespace Hash_Map.HashFunctions.HashFucntionsForOpenAdress.HashFunc
 {
     internal class XorHashForInt : IOpenAdressHashFunc
     {
+        byte[,] S = new byte[8,16]{ // S-блоки, используемые ЦБ РФ
+                        { 4, 10,  9,  2, 13,  8,  0, 14,  6, 11,  1, 12,  7, 15,  5,  3},
+                        {14, 11,  4, 12,  6, 13, 15, 10,  2,  3,  8,  1,  0,  7,  5,  9},
+                        { 5,  8,  1, 13, 10,  3,  4,  2, 14, 15, 12,  7,  6,  0,  9, 11},
+                        { 7, 13, 10,  1,  0,  8,  9, 15, 14,  4,  6, 12, 11,  2,  5,  3},
+                        { 6, 12,  7,  1,  5, 15, 13,  8,  4, 10,  9, 14,  0,  3, 11,  2},
+                        { 4, 11, 10,  0,  7,  2,  1, 13,  3,  6,  8,  5,  9, 12, 15, 14},
+                        {13, 11,  4,  1,  3, 15,  5,  9,  0, 10, 14,  7,  6,  8,  2, 12},
+                        { 1, 15, 13,  0,  5,  7, 10,  4,  9,  2,  3, 14,  6, 11,  8, 12}};
         public string GetName()
         {
             return "Хэш функции с побитовым сдвигом и с побитовым логическим исключающим ИЛИ";
@@ -36,15 +47,63 @@ namespace Hash_Map.HashFunctions.HashFucntionsForOpenAdress.HashFunc
                 keys[i] = FunctionP(W);
             }
 
+            StringBuilder encValueParts = new StringBuilder();
+            for (int i = 0; i < 4; i++) {
+                encValueParts.Append(Encryption(Hin.Substring(64 * i, 64), keys[i]));
+            }
+
+            string encValue = encValueParts.ToString();
+
             
-            return 0;
         }
+
+        private string Encryption(string value, string key)
+        {
+            char[] firstPart = value.Substring(0,32).ToCharArray();
+            char[] secondPart = value.Substring(32, 32).ToCharArray();
+
+            
+            for (int step = 0; step < 3; step++) {       // K1..K24 идут в прямом порядке - три цикла K1..K8
+                for (int j = 0; j < 8; j += 1)
+                {
+                    char[] temp = new char[32];
+                    firstPart.CopyTo(temp, 0);
+                    
+                    firstPart = Convert.ToString(Convert.ToInt32(new string(secondPart),2) ^ EncryptionFunc(firstPart,key.Substring(j*32,32)),2).ToCharArray();
+                    secondPart = temp;
+                }
+            }
+
+            for (int i = 7; i >= 0; i -= 1)
+            {
+                char[] temp = new char[32];
+                firstPart.CopyTo(temp, 0);
+
+                firstPart = Convert.ToString(Convert.ToInt32(new string(secondPart), 2) ^ EncryptionFunc(firstPart, key.Substring(i * 32, 32)), 2).ToCharArray();
+                secondPart = temp;
+            }
+
+            return new string(firstPart) + new string(secondPart);
+        }
+
+        private int EncryptionFunc(char[] bitsOfNumber, string key)
+        {
+            uint value = (uint)((Convert.ToInt64(new string(bitsOfNumber), 2) + Convert.ToInt64(key.ToString(),2))%4294967296);
+            string bitValue = Convert.ToString(value, 16);
+            StringBuilder result = new StringBuilder();
+            for(int i = 0; i < 8; i++)
+            {   
+                result.Append(Convert.ToString(S[i, Convert.ToInt32(bitValue[i].ToString(), 16)],16));
+            }
+            return Convert.ToInt32(result.ToString(), 16)<<11;
+        }
+
         private string XorForStrings(string value1,string value2)
         {
             StringBuilder result = new StringBuilder();
             for (int i = 3; i >= 0; i--)
             {
-                result.Append((long.Parse(value1.Substring(i*64, 64)) ^ long.Parse(value2.Substring(i*64, 64))).ToString());
+                result.Append((Convert.ToInt64(value1.Substring(i*64, 64)) ^ Convert.ToInt64(value2.Substring(i*64, 64))).ToString());
             }
            
             return result.ToString();
