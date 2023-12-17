6be364a52c3a09fd1ad89aa01057d46a9145adb9
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-12-17 12:07:47 +0500

    SHA256

---
+++ Hash-Map/HashFunctions/HashFunctionsForChinedHashMap/HashFunc/MD5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Map.HashFunctions.HashFunctionsForChinedHashMap.HashFunc
+{
+    public class MD5 : IChinedHashFunc
+    {
+
+        public string GetName() => "Хеш-функция MD5";
+
+        private int Start(object data, int size)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+
+                byte[] hash = ObjectToByteArray(data);
+                byte[] hashBytes = md5.ComputeHash(hash);
+                return Math.Abs(BitConverter.ToInt32(md5.ComputeHash(hashBytes))%size);
+            }
+        }
+
+        public Func<object, int, int> GetHashFunc()
+        {
+            return Start;
+        }
+
+        private static byte[] ObjectToByteArray(Object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+    }
+}
+
